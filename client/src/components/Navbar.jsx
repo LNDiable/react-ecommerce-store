747e8a85db84de15949b3eb4b7d1d6bc64dc3b61
Luke <LNDiable@gmail.com>
2026-07-02 14:06:49 +0100

    Create Navbar Component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import App from "../App"
+
+
+function Navbar()
+{
+    return(
+        
+        <nav className='navbar'>
+            <h1>E-comms Website</h1>
+            <a href="#">Home</a>
+            <a href="#">Products</a>
+            <a href="#">Gallery</a>
+            <a href="#">About us</a>
+            <a href="#">Login</a>
+            
+        </nav>
+        
+    )
+
+    
+}
+export default Navbar
